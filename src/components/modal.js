6a02a850f3539c80fa4df0a9d7257386f5865b15
Mehmet Yener YILMAZ <yilmazmehmetyener@gmail.com>
2022-11-11 14:29:52 +0300

    pass props to modal

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,29 +1,58 @@
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import Table from 'react-bootstrap/Table';
 
-function MyModal(props) {
+function MyModal({ title, description, price, discountPercentage, rating, stock, brand, category, onHide, show}) {
+//const { title, description, price, discountPercentage, rating, stock, brand, category,onHide ,show} = props
   return (
     <Modal
-      {...props}
+    show={show}
       size="lg"
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
+          {title}({stock})
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
+        <Table striped bordered hover>
+          <caption>
+            {description}
+          </caption>
+          <thead>
+          <tr>
+              <th>Tanım</th>
+              <th>Deger</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr onClick={()=>{alert("table clicked!")}}>
+              <td>price</td>
+              <td>{price}</td>
+            </tr>
+            <tr>
+              <td>discountPercentage</td>
+              <td>{discountPercentage}</td>
+            </tr>
+            <tr>
+              <td>rating</td>
+              <td>{rating}</td>
+            </tr>
+            <tr>
+              <td>brand</td>
+              <td>{brand}</td>
+            </tr>
+            <tr>
+              <td>category</td>
+              <td>{category}</td>
+            </tr>
+          </tbody>
+        </Table>
       </Modal.Body>
       <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
+        <Button onClick={onHide}>Close</Button>
       </Modal.Footer>
     </Modal>
   );
